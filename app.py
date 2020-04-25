70366b8c9b772eee91500d8e790b7de91bf6cadc
Jose Nieto <dnbit@users.noreply.github.com>
2020-04-25 23:59:37 +0100

    Add Show table

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ class Venue(db.Model):
     facebook_link = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean(), default=True)
     seeking_description = db.Column(db.String(250))
+    shows = db.relationship('Show', backref='venue')
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
@@ -60,21 +60,17 @@ class Artist(db.Model):
     facebook_link = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean(), default=False)
     seeking_description = db.Column(db.String(250))
-    # past_shows:
-    # upcoming_shows:
-    # past_shows_count:
-    # upcoming_shows_count: 
+    shows = db.relationship('Show', backref='artist')
 
-# class Show(db.Model):
-#     __tablename__ = 'Show'
 
-#     id = db.Column(db.Integer, primary_key=True)
+class Show(db.Model):
+    __tablename__ = 'Show'
 
-db.create_all()
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime, nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable = False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable = False)
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
 #----------------------------------------------------------------------------#
 # Filters.
